cc45d5d479d33bc3c2dbd2cde769e20e9b594761
Raqhu <88824509+Raqhu@users.noreply.github.com>
2021-08-14 15:34:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,56 @@
-var weight = [17,25,39,44,56]
-function avrg() {
-  var sum1 = 0;
-  for(var i = 0; i<4; i = i+1){
-   sum1 = sum+weight [i];
-  }
-   var avg = sum/weight.length;
-   console.log(avg);
+//declaring global variables
+var trex, trex_running, edges;
+var groundImage;
+var ground;
+
+function preload(){
+  // loading animation for trex
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  //loading image for ground
+  groundImage = loadImage("ground2.png")
+}
+
+function setup(){
+  createCanvas(600,200);
   
+  // creating trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  edges = createEdgeSprites();
+  
+  //adding scale and position to trex
+  trex.scale = 0.5;
+  trex.x = 50
+
+  //making the ground
+  ground = createSprite(300,180,600,20);
+  ground.x = ground.width/2;
+  ground.addImage("ground1",groundImage)
 }
 
-function setup() 
-{
-  createCanvas(400,400);
-  //avrg();
-  var sum = weight[0] + weight[1] + weight[2] + weight[3] + weight[4];
-  var avg1 = sum/ weight.length;
 
-  console.log(avg1);
+function draw(){
+  //set background color 
+  background("white");
+  //adding movement for the ground
+  ground.velocityX = -4;
+//infinite ground by resetting
+  if(ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //logging the y position of the trex
+  console.log(trex.y)
+  
+  //jump when space key is pressed
+  if(keyDown("space")){
+    trex.velocityY = -10;
+  }
+  
+  //adding an effect of gravity for trex
+  trex.velocityY = trex.velocityY + 0.5;
+  
+  //stop trex from falling down
+  trex.collide(ground);
+  drawSprites();
 }
-
-function draw() 
-{
-  background(51);
-
-}
-
